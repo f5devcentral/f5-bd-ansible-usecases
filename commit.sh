cd69bc4686b193947d9aaac33c8b0fd3b863bbd7
vdi-tech-guy <m.mabis@f5.com>
2021-02-24 00:14:33 +0000

    Code Updates - Wed Feb 24 00:14:33 UTC 2021

--- commit.sh
+++ commit.sh
@@ -2,4 +2,4 @@ git config --global user.email "m.mabis@f5.com"
 git config --global user.name "vdi-tech-guy"
 git add -A
 git commit -m "Code Updates - $(date)"
-git push https://vdi-tech-guy@github.com/VDI-Tech-Guy/f5-bd-ansible-usecases-AS3.git
+git push https://github.com/f5devcentral/f5-bd-ansible-usecases.git
